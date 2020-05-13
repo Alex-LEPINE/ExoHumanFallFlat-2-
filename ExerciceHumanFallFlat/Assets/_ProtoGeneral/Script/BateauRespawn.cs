50a4a0471d75af178a10497ca5a41863e04fd244
Alex <a.lepine@rubika-edu.com>
2020-05-13 18:17:08 +0200

    Fix de Bugs

---
+++ ExerciceHumanFallFlat/Assets/_ProtoGeneral/Script/BateauRespawn.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BateauRespawn : MonoBehaviour 
+{
+    Vector3 IniPos;
+    Quaternion IniRot;
+    Rigidbody rb;
+
+    void Start() 
+    {
+        IniPos = gameObject.transform.position;
+        IniRot = gameObject.transform.rotation;
+    }
+    void OnTriggerExit(Collider col)
+    {
+        if (col.gameObject.layer == 9)
+        {
+            gameObject.transform.position = IniPos;
+            gameObject.transform.rotation = IniRot;
+            rb.velocity = new Vector3(0f, 0f, 0f);            
+        }
+    }
+}
---
+++ ExerciceHumanFallFlat/Assets/_ProtoGeneral/Script/BouleTrigger.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BouleTrigger : MonoBehaviour 
+{
+    Rigidbody rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+
+    }
+
+
+    void OnTriggerEnter(Collider col)
+    {
+        if (col.gameObject.layer == 9)
+        {
+            Debug.Log(col);
+            rb.constraints = RigidbodyConstraints.None;
+        }
+    }
+}
